1fdd847d94f3426bd4ccbc3e0fa7cbea336eec36
unknown <rubikonspace@ukr.net>
2023-10-09 13:41:35 +0300

    css

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 export const ContainerWrapper = styled.div`
   /* margin-bottom: 10px; */
   /* display: flex; */
-  /* margin: 0 auto; */
+  margin: 0 auto;
   width: 800px;
   border: 2px solid #cccccc;
   border-radius: 10px;
   background-color: #f5f5f5;
-  margin-left: 15px;
 `;
 
 export const TitlePhonebook = styled.h1`
